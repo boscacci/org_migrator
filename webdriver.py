f620b2f864992e1c568ea0b8942781bb260a5e6d
Robert Boscacci <rob@Roberts-MacBook-Air.local>
2019-02-12 17:08:24 -0500

    First few functions built

--- webdriver.py
+++ webdriver.py
@@ -1,28 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import re
+
+def page_check():
+	try:
+		driver.find_element_by_link_text("Next")
+		return True
+	except:
+		return False
 
 def load_repos(username):
+	"""Returns list of repo_names"""
 	
 	driver = webdriver.Chrome()
-	driver.get(f"https://github.com/{username}?tab=repositories")
+	driver.get(f"https://github.com/{username}?utf8=%E2%9C%93&tab=repositories&q=&type=public&language=python")
 	
 	repo_names = []
 
-	while driver.find_element_by_link_text("Next"):
+	while True:
 
-		repo_names.extend([repo_name.text for repo_name in 
-		driver.find_elements_by_tag_name("h3")][2:])
+		repo_names.extend([repo_name.text for repo_name in driver.find_elements_by_tag_name("h3")][2:])
 
-		next_link = driver.find_element_by_link_text("Next")
+		if page_check():
 
-		next_link.click()
+			next_link = driver.find_element_by_link_text("Next")
 
-	import pdb; pdb.set_trace()
+			next_link.click()
 
-	elem.clear()
-	elem.send_keys("pycon")
-	elem.send_keys(Keys.RETURN)
-	
-	assert "No results found." not in driver.page_source
+			driver.implicitly_wait(1)
+
+		else: 
+			break
 
 	driver.close()
+
+	return repo_names
+
+def filter_repos(repo_names, search_string):
+	desired_repos = []
+	for repo_name in repo_names:
+		if search_string in repo_name:
+			desired_repos.append(repo_name)
+	return desired_repos
+
+import pdb; pdb.set_trace()
